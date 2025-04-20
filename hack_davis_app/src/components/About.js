f0d44d5b7e8cc2573ba9516362511244be62b175
Andy Tran <TylerT124@yahoo.com>
2025-04-20 08:07:16 -0700

    Updated about and put placeholder image

--- hack_davis_app/src/components/About.js
+++ hack_davis_app/src/components/About.js
@@ -1,21 +1,51 @@
+import './About.css'
+import TestImage from "../images/logo192.png"
+
+function AboutHeader(){
+    return(
+        <>
+        <div className='image-header'>
+            <img src={TestImage} alt='stock of fire victims'/>
+            <p>
+                WeRemain is a <b>community-driven</b> platform where disaster survivors share memories of 
+                what they’ve lost and the resources that helped them recover. 
+                <br/>
+                <br/>
+                By combining personal stories with practical insights, 
+                <b>WeRemain turns lived experience into a guide for others navigating similar challenges,</b>
+                helping survivors feel seen, supported, and better equipped to rebuild.
+            </p>
+        </div>
+        </>
+    );
+   
+}
+
 function About(){
     return(
         <>
         <div className="MASTER">
-            <h1>About us...</h1>
-            <p>
-                WeRemain is a community-driven platform where disaster survivors share memories of 
-                what they’ve lost and the resources that helped them recover. 
-                By combining personal stories with practical insights, 
-                WeRemain turns lived experience into a guide for others navigating similar challenges, 
-                helping survivors feel seen, supported, and better equipped to rebuild.
-            </p>
-            <ol>
-                <li>Share Your Story</li>
-                <li>Highlight What Helped</li>
-                <li>Connect Through Stories</li>
-            </ol>
+            <h1 className='About-h1'>About</h1>
+            <AboutHeader/>
+            <div className='about-block-wrapper'>
+                <div className='about-block'>
+                    <h2>1</h2>
+                    <p>Share Your Story</p>
+                    <p>Upload photos and memories of what was lost.</p>
+                </div>
+                <div className='about-block'>
+                    <h2>2</h2>
+                    <p>Highlight What Helped</p>
+                    <p>Highlight the resources, programs, or people that supported you in recovery.</p>
+                </div>
+                <div className='about-block'>
+                    <h2>3</h2>
+                    <p>Connect Through Stories</p>
+                    <p>Explore others’ experiences to feel seen, build community, and find support.</p>
+                </div>
+            </div>
         </div>
+            
         </>
     );
 }
