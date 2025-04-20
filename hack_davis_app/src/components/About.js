3103b1e7a05a03b93a5ff70f98a9d3959f968e44
Andy Tran <TylerT124@yahoo.com>
2025-04-20 08:48:08 -0700

    Updated colors for about

--- hack_davis_app/src/components/About.js
+++ hack_davis_app/src/components/About.js
@@ -11,9 +11,7 @@ function AboutHeader(){
                 what they’ve lost and the resources that helped them recover. 
                 <br/>
                 <br/>
-                By combining personal stories with practical insights, 
-                <b>WeRemain turns lived experience into a guide for others navigating similar challenges,</b>
-                helping survivors feel seen, supported, and better equipped to rebuild.
+                By combining personal stories with practical insights, <b>WeRemain turns lived experience into a guide for others navigating similar challenges,</b> helping survivors feel seen, supported, and better equipped to rebuild.
             </p>
         </div>
         </>
@@ -26,21 +24,21 @@ function About(){
         <>
         <div className="MASTER">
             <h1 className='About-h1'>About</h1>
-            <AboutHeader/>
+            <AboutHeader className='AboutHeader'/>
             <div className='about-block-wrapper'>
                 <div className='about-block'>
-                    <h2>1</h2>
-                    <p>Share Your Story</p>
+                    <h2 className='About-h2'>1</h2>
+                    <h3>Share Your Story</h3>
                     <p>Upload photos and memories of what was lost.</p>
                 </div>
                 <div className='about-block'>
-                    <h2>2</h2>
-                    <p>Highlight What Helped</p>
+                    <h2 className='About-h2'>2</h2>
+                    <h3>Highlight What Helped</h3>
                     <p>Highlight the resources, programs, or people that supported you in recovery.</p>
                 </div>
                 <div className='about-block'>
-                    <h2>3</h2>
-                    <p>Connect Through Stories</p>
+                    <h2 className='About-h2'>3</h2>
+                    <h3>Connect Through Stories</h3>
                     <p>Explore others’ experiences to feel seen, build community, and find support.</p>
                 </div>
             </div>
