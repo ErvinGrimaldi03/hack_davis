8c4d6e1d283f1131a08b6aaecf7b7b3c5c69ee14
ErvinGrimaldi03 <grimaldiervin@gmail.com>
2025-04-20 09:19:29 -0700

    NavBar design changed

--- hack_davis_app/src/components/Navbar.js
+++ hack_davis_app/src/components/Navbar.js
@@ -4,34 +4,36 @@ import './Navbar.css';
 function NavBar() {
     const [isOpen, setIsOpen] = useState(false);
 
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
-const toggleMenu = () => {
-    setIsOpen(!isOpen);
-};
+    const handleScroll = (sectionId) => {
+        const section = document.getElementById(sectionId);
+        if (section){
+            section.scrollIntoView({ behavior: 'smooth' });
+            setIsOpen(false);
+        }
+    };
 
-const handleScroll = (sectionId) => {
-    const section = document.getElementById(sectionId);
-    if (section){
-        section.scrollIntoView({ behavior: 'smooth' });
-        setIsOpen(false);
-    }
-};
-
-return (
-    <nav className="NavBar"> 
-        <h1> Insert Name Here Because I have no Fucking idea on how to call this bitchass Application! </h1>
-        <ul className={`NavLink ${isOpen ? 'active' : ''}`}> 
-            <li><a href="#about" onClick={() => handleScroll('about')}>About</a></li>
-            <li><a href="#stories" onClick={() => handleScroll('stories')}>Stories</a></li>
-            <li><a href="#resources" onClick={() => handleScroll('resources')}>Resources</a></li>
-        </ul>
-        <div className="hamburger" onClick={toggleMenu}>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    </nav>
-);
+    return (
+        <nav className="NavBar"> 
+            <div className="logo">
+                <h1>WeRemain</h1>
+            </div>
+            <ul className={`NavLinks ${isOpen ? 'active' : ''}`}> 
+                <li><a href="#stories" onClick={() => handleScroll('stories')}>Stories</a></li>
+                <li><a href="#about" onClick={() => handleScroll('about')}>About</a></li>
+                <li><a href="#resources" onClick={() => handleScroll('resources')}>Resources</a></li>
+                <li className="share-button"><a href="#share" onClick={() => handleScroll('share')}>Share Your Story</a></li>
+            </ul>
+            <div className="hamburger" onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </nav>
+    );
 }
 
-export default NavBar; 
+export default NavBar;
